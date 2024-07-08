2ed0cb468b4f05360da4e789fe7e525ff7c14d99
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-09 00:44:59 +0530

    ArraysCompareTest

---
+++ src/test/java/com/junit/ArraysCompareTest.java
@@ -0,0 +1,28 @@
+package com.junit;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArraysCompareTest {
+
+    // Arrays.sort
+
+    @Test
+    public void testArraySort_RandomArray(){
+        int[] numbers = {12,3,4,1};
+        int[] expected = {1,3,4,12};
+//        int[] expected = {1,4,3,12};
+        Arrays.sort(numbers);
+        assertArrayEquals(expected,numbers);
+        // array contents differ at index [1], expected: <4> but was: <3>
+    }
+    @Test(expected=NullPointerException.class)
+    public void testArraySort_NullArray(){
+        int[] numbers = null;
+        Arrays.sort(numbers);
+    }
+}
