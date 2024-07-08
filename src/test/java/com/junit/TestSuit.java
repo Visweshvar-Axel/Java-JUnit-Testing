e1509dc9c564845c8d0ada853d60346d0c9c1e47
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-09 02:44:39 +0530

    TestSuit

---
+++ src/test/java/com/junit/TestSuit.java
@@ -0,0 +1,10 @@
+package com.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SampleTest.class})
+public class TestSuit {
+
+}
