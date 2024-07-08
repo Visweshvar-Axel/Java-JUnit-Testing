5e3752edb3dbf11b522588099f9cc514ca14baaa
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-08 21:03:26 +0530

    StringHelperTest

--- src/test/java/com/junit/StringHelperTest.java
+++ src/test/java/com/junit/StringHelperTest.java
@@ -5,22 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class StringHelperTest {
+	StringHelper helper = new StringHelper();
 
-	@Test
-	void testtruncateAInFirst2Positions_Ainfirst2position() {
-		StringHelper helper = new StringHelper();
-		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
 //		before inline refactor
 //		String actual = helper.truncateAInFirst2Positions("AACD");
 //		String expected = "CD";
 //		assertEquals(expected,actual);
 		//AACD -> CD ACD -> CD CDAA -> CDAA
 	   //expected: <ABC> but was: <Abc>
+	@Test
+	void test_truncateAInFirst2Positions_AinFirst2position() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
 	}
 
 	@Test
-	public void testtruncateAInFirst2Positions_Ainfirstposition() {
-		StringHelper helper = new StringHelper();
+	public void test_truncateAInFirst2Positions_AinFirstPosition() {
 		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+	@Test
+	public void test_truncateAInFirst2Positions_withoutA() {
+		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+	}
+	@Test
+	public void test_truncateAInFirst2Positions_withANotInFirst2Positions() {
+		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
 	}
 }
