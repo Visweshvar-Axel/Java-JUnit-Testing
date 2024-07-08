e406b183ca9c38b5c9a5a6387940c36534cf3f5f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-09 02:44:22 +0530

    SampleTest

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SampleTest {
     @Test
-    void factTest1(){
+    public void factTest1(){
         Sample sample = new Sample();
         int res = sample.fact(5);
         assertEquals(120,res,"the result should be 120 but wrong output: "+res);
     }
     @Test
-    void factTest2(){
+    public void factTest2(){
         Sample sample = new Sample();
         int res = sample.fact(4 );
         assertEquals(24,res,"the result should be 120 but wrong output: "+res);
