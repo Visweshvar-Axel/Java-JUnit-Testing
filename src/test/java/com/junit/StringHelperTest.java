0aa8052ccae9f928ea07dfc825282d4190e689f7
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-08 20:59:21 +0530

    StringHelperTest

--- src/test/java/com/junit/StringHelperTest.java
+++ src/test/java/com/junit/StringHelperTest.java
@@ -7,16 +7,20 @@ import org.junit.jupiter.api.Test;
 class StringHelperTest {
 
 	@Test
-	void test() {
+	void testtruncateAInFirst2Positions_Ainfirst2position() {
 		StringHelper helper = new StringHelper();
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
 //		before inline refactor
 //		String actual = helper.truncateAInFirst2Positions("AACD");
 //		String expected = "CD";
 //		assertEquals(expected,actual);
 		//AACD -> CD ACD -> CD CDAA -> CDAA
-		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 	   //expected: <ABC> but was: <Abc>
 	}
 
+	@Test
+	public void testtruncateAInFirst2Positions_Ainfirstposition() {
+		StringHelper helper = new StringHelper();
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
 }
