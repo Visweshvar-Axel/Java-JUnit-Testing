0aa156f46007b6b52c606e21dc7b362d59e2fe21
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-08 16:41:08 +0530

    StringHelperTest

---
+++ src/test/java/com/junit/StringHelperTest.java
@@ -0,0 +1,18 @@
+package com.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringHelperTest {
+
+	@Test
+	void test() {
+		StringHelper helper = new StringHelper();
+		String actual = helper.truncateAInFirst2Positions("AACD");
+		String expected = "CD";
+		assertEquals(expected,actual);
+	}
+   //	expected: <ABC> but was: <Abc>
+
+}
