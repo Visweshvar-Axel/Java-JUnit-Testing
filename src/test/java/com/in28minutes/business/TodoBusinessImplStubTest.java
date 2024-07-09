da20accc6d413b40d8a932452cdd8fa07128c0ec
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-09 20:46:32 +0530

    Stub

---
+++ src/test/java/com/in28minutes/business/TodoBusinessImplStubTest.java
@@ -0,0 +1,30 @@
+package com.in28minutes.business;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.in28minutes.data.api.TodoService;
+import com.in28minutes.data.stub.TodoServiceStub;
+
+public class TodoBusinessImplStubTest {
+
+	@Test
+	public void usingAStub() {
+		TodoService todoService = new TodoServiceStub();
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		List<String> todos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Ranga");
+		assertEquals(2, todos.size());
+	}
+	@Test
+	public void usingAStub_dummy() {
+		TodoService todoService = new TodoServiceStub();
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		List<String> todos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Dummy");
+		assertEquals("Learn Spring MVC , Learn Spring", todos.get(0)+" , "+todos.get(1));
+	}
+}
