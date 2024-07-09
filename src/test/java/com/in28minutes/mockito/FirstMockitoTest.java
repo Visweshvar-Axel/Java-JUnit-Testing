2ce0a5728120bcf0ad011289948f346e0dcc3553
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-09 17:15:17 +0530

    mockito

---
+++ src/test/java/com/in28minutes/mockito/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstMockitoTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
