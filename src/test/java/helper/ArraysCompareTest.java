a18eda3b216feee5df7cb9e654819371bb8fde52
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-10 13:01:28 +0530

    mockito

---
+++ src/test/java/helper/ArraysCompareTest.java
@@ -0,0 +1,55 @@
+package helper;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArraysCompareTest {
+
+    // Arrays.sort
+
+    @Test
+    public void testArraySort_RandomArray(){
+        int[] numbers = {12,3,4,1};
+        int[] expected = {1,3,4,12};
+//        int[] expected = {1,4,3,12};
+        Arrays.sort(numbers);
+        assertArrayEquals(expected,numbers);
+        // array contents differ at index [1], expected: <4> but was: <3>
+    }
+//    old version
+//    @Test(expected=NullPointerException.class)
+//    public void testArraySort_NullArray(){
+//        int[] numbers = null;
+//        Arrays.sort(numbers);
+//    }
+    @Test
+    public void testArraySort_NullArray(){
+        int[] numbers = null;
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+            Arrays.sort(numbers);
+        });
+//        Arrays.sort(numbers);
+        assertNotNull(exception);
+    }
+//    old version
+//    @Test(timeout=1000)
+//    public void testSort_Performance(){
+//        int array[] = {12,23,4}
+//        for (int i = 1; i < 1000000; i++) {
+//            array[0] = i;
+//            Arrays.sort(array);
+//        }
+//    }
+    @Test
+    public void testSort_Performance(){
+        int array[] = {12,23,4};
+        assertTimeout(java.time.Duration.ofMillis(1000),()->{
+            for (int i = 1; i < 1000000; i++) {
+                array[0] = i;
+                Arrays.sort(array);
+            }
+        });
+    }
+}
