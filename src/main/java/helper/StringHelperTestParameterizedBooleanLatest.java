8afb6c4f84264d0be5c463919c777fee27cc8ed9
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-10 15:14:32 +0530

    StringHelperTestParameterizedBooleanLatest

---
+++ src/main/java/helper/StringHelperTestParameterizedBooleanLatest.java
@@ -0,0 +1,21 @@
+package helper;
+
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringHelperTestParameterizedBooleanLatest {
+
+    StringHelper helper = new StringHelper();
+
+//    @ParameterizedTest
+//    @ValueSource(strings = {"ABCD,false", "ABAB,true", "AB,true", "A,false"})
+//    public void testAreFirstAndLastTwoCharactersTheSame(String inputExpected) {
+//        String[] parts = inputExpected.split(",");
+//        String input = parts[0];
+//        boolean expected = Boolean.parseBoolean(parts[1]);
+//
+//        assertEquals(expected, helper.areFirstAndLastTwoCharactersTheSame(input));
+//    }
+}
