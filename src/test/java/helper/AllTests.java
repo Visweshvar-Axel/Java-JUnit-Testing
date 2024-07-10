95e87a864538f4dbc88296a60fc3d7493b44aae2
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-10 13:01:25 +0530

    mockito

---
+++ src/test/java/helper/AllTests.java
@@ -0,0 +1,12 @@
+package helper;
+//
+//import org.junit.platform.suite.api.SelectClasses;
+//import org.junit.platform.suite.api.Suite;
+//
+//@Suite
+//@SelectClasses({ ArraysCompareTest.class, QuickBeforeAfterTest.class, StringHelperParameterizedTest.class,
+//		StringHelperParameterizedTestLatest.class, StringHelperTest.class, StringHelperTestParameterizedBoolean.class,
+//		StringHelperTestParameterizedBooleanLatest.class })
+public class AllTests {
+
+}
