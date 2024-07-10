d6f81d0009351928970170c68654e2ee5446e997
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-10 15:14:15 +0530

    StringHelperParameterizedTestLatest

---
+++ src/test/java/helper/StringHelperParameterizedTestLatest.java
@@ -0,0 +1,18 @@
+package helper;
+
+//import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import javax.management.ConstructorParameters;
+
+import static org.junit.Assert.*;
+
+public class StringHelperParameterizedTestLatest {
+    StringHelper helper = new StringHelper();
+
+//    @ParameterizedTest
+//    @CsvSource({ "AACD, CD", "ACD, CD" })
+//    void testTruncateAInFirst2Positions(String input, String expected) {
+//        assertEquals(expected, helper.truncateAInFirst2Positions(input));
+//    }
+}
