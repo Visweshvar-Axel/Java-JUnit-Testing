f6cc9610ea75c906fb77800015e4d841fe8919f9
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-10 17:25:03 +0530

    ListTest

---
+++ src/test/java/mockito/ListTest.java
@@ -0,0 +1,60 @@
+package mockito;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ListTest {
+
+	@Test
+	public void letsMockListSize() {
+		List list = mock(List.class);
+		Mockito.when(list.size()).thenReturn(10);
+		assertEquals(10, list.size());
+	}
+
+	@Test
+	public void letsMockListSizeWithMultipleReturnValues() {
+		List list = mock(List.class);
+		Mockito.when(list.size()).thenReturn(10).thenReturn(20);
+		assertEquals(10, list.size()); 
+		assertEquals(20, list.size()); 
+	}
+
+	@Test
+	public void letsMockListGet() {
+		List<String> list = mock(List.class);
+		Mockito.when(list.get(0)).thenReturn("viswa");
+		assertEquals("viswa", list.get(0));
+		assertNull(list.get(1));
+	}
+
+	@Test
+	public void letsMockListGetWithAny() {
+		List<String> list = mock(List.class);
+		Mockito.when(list.get(Mockito.anyInt())).thenReturn("viswa");
+		assertEquals("viswa", list.get(0));
+		assertEquals("viswa", list.get(1));
+	}
+
+	@Test
+	public void bddAliases_UsingGivenWillReturn() {
+		//given
+		List<String> list = mock(List.class);
+		given(list.get(Mockito.anyInt())).willReturn("viswa");
+
+		//when
+		String actual = list.get(0);
+
+		//then
+		assertThat(actual, is("viswa"));
+	}
+}
